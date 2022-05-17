81ba171dfec564f08cca09c0331738b1dc43059c
Mariusz <mariusz.wluka@simplio.io>
2022-05-17 21:35:51 +0200

    Apply tests changes

---
+++ integration_test/app_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:simplio_app/main.dart' as app;
+
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  group('end-to-end test', () {
+    testWidgets('Simple test for beginning',
+            (WidgetTester tester) async {
+          app.main();
+          await tester.pumpAndSettle();
+          tester.printToConsole(DateTime.now().toString() + ' ' + 'Tests started');
+          expect(find.text('You have no wallet'), findsOneWidget);
+          expect(find.text('Wallets'), findsOneWidget);
+          tester.printToConsole(DateTime.now().toString() + ' ' + 'Tests ended');
+        });
+  });
+}
