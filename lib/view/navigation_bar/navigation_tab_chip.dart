e72defcea1bca339b845ec5d9a0c390fd9dc3edd
Tomáš Petrík <tomas.petrik@kbc.ie>
2022-08-19 10:55:39 +0200

    Removed unused import

--- lib/view/navigation_bar/navigation_tab_chip.dart
+++ lib/view/navigation_bar/navigation_tab_chip.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:simplio_app/view/themes/common_theme.dart';
 import 'package:simplio_app/view/themes/constants.dart';
 import 'package:simplio_app/view/themes/simplio_colors.dart';
 import 'package:simplio_app/view/themes/simplio_text_styles.dart';
